c053d279cfe174149d57b449593416004a6a333d
Dr. Doris Chen <doristchen@hotmail.com>
2017-03-03 11:16:37 -0800

    converted to typescript

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## Getting Started
 1. Create a `.env` file from the `.env.example` file. And fill in the information
-2. Simply run `npm start`
+2. If you modify the js/index.ts, please use "tsc" to compile the project at root level (README.md level)
+2. Simply run `npm start`at root level (README.md level)
---
+++ public/build/index.js
@@ -0,0 +1,73 @@
+/* global requestAnimationFrame, fetch, URL */
+const drawRect = (canvasCtx, dimensions) => {
+    const { x, y, w, h } = dimensions;
+    canvasCtx.strokeRect(x, y, w, h);
+};
+const onSuccessMedia = (stream) => {
+    const streamVideo = document.querySelector('#stream');
+    const feedCanvas = document.querySelector('#feed');
+    const { width, height } = feedCanvas;
+    const feedContext = feedCanvas.getContext('2d');
+    const videoSourceUrl = window.URL.createObjectURL(stream);
+    document.querySelector('#trainButton').addEventListener('click', () => {
+        const personIdInput = document.querySelector('#personId');
+        const personId = personIdInput.value;
+        fetch('/train', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                data: feedCanvas.toDataURL('image/png'),
+                personId
+            })
+        }).then((res) => {
+            return res.json();
+        }).then((data) => {
+            console.log(data);
+        });
+    });
+    document.querySelector('#identifyButton').addEventListener('click', () => {
+        fetch('/identify', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                data: feedCanvas.toDataURL('image/png')
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+            console.log(data);
+        });
+    });
+    console.log('videoSource', videoSourceUrl, width, height);
+    streamVideo.autoplay = true;
+    streamVideo.src = videoSourceUrl;
+    feedContext.drawImage(streamVideo, 0, 0, width, height);
+    const renderFeedCanvas = () => {
+        feedContext.drawImage(streamVideo, 0, 0, width, height);
+        // drawRect(feedContext, {x: 50, y: 50, w: 100, h: 100})
+        requestAnimationFrame(renderFeedCanvas);
+    };
+    requestAnimationFrame(renderFeedCanvas);
+    // setTimeout(() => {
+    //   fetch('/submit', {
+    //     method: 'post',
+    //     headers: {
+    //       'Content-type': 'application/json; charset=UTF-8'
+    //     },
+    //     body: JSON.stringify({
+    //       data: feedCanvas.toDataURL('image/png')
+    //     })
+    //   }).then((res) => {
+    //     return res.json()
+    //   }).then((data) => {
+    //     console.log(data)
+    //   })
+    // }, 3000)
+    const feedImage = feedContext.getImageData(0, 0, width, height);
+    console.log(feedImage);
+};
+navigator.getUserMedia({ 'video': true }, onSuccessMedia, () => { });
